a36369302ece3f61ce5bb74f4e6fb92ece66f424
Kishan <novasuper11@proton.me>
2025-07-31 03:37:51 +0530

    update webpack configuration

--- interface/webpack.config.js
+++ interface/webpack.config.js
@@ -1,36 +1,30 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const path = require('path');
 const dotenv = require('dotenv');
 
-const env = dotenv.config().parsed || {};
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+// Load .env file
+const env = dotenv.config({ path: path.resolve(__dirname, '.env') }).parsed;
+
+// Prepare environment variables for DefinePlugin
+const envKeys = Object.keys(env || {}).reduce((prev, key) => {
+  prev[`process.env.${key}`] = JSON.stringify(env[key]);
   return prev;
 }, {});
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  devtool: 'inline-source-map',
+  mode: 'development',
   devServer: {
     static: path.join(__dirname, 'dist'),
     compress: true,
     port: 8080,
-    open: false,
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-    }),
-    new webpack.DefinePlugin(envKeys),
-  ],
   module: {
     rules: [
       {
@@ -39,11 +33,10 @@ module.exports = {
       },
     ],
   },
-  resolve: {
-    fallback: {
-      buffer: require.resolve('buffer'),
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-    },
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new webpack.DefinePlugin(envKeys),
+  ],
 };
