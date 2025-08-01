f48a608ddf6b0ac9d6ec66b84e41575ecdad81ab
Kishan <novasuper11@proton.me>
2025-08-02 01:07:30 +0530

    add sepolia network to config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, vars } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -7,7 +10,12 @@ const config: HardhatUserConfig = {
     hardhat: {
       chainId: 1337,
     }
-  }
+  },
+  sepolia: {
+    url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+    accounts: [SEPOLIA_PRIVATE_KEY],
+    chainId: 11155111,
+  },
 };
 
 export default config;
